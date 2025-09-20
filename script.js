02e497d1d9eea679692bea88b3f2336424da431b
Claudia <claudia_4bcn@hotmail.com>
2025-09-20 21:19:59 +0200

    Add FAQ

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-document.querySelectorAll('.tab').forEach(tab => {
+document.querySelectorAll(".tab").forEach((tab) => {
   tab.onclick = () => {
-    document.querySelectorAll('.tab, .tab-panel').forEach(el => el.classList.remove('active'));
-    tab.classList.add('active');
-    document.getElementById(tab.dataset.tab).classList.add('active');
+    document
+      .querySelectorAll(".tab, .tab-panel")
+      .forEach((el) => el.classList.remove("active"));
+    tab.classList.add("active");
+    document.getElementById(tab.dataset.tab).classList.add("active");
   };
+});
+
+const questions = document.querySelectorAll(".faq-question");
+
+questions.forEach((q) => {
+  q.addEventListener("click", () => {
+    const answer = q.nextElementSibling;
+    answer.style.display = answer.style.display === "block" ? "none" : "block";
+  });
 });
